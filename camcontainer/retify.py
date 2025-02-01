15ca5332dde3a9a2eca99556afd31e9d7f997d92
Ndu <nduonyiukitetu@gmail.com>
2025-02-01 15:45:54 -0800

    we did it

--- camcontainer/retify.py
+++ camcontainer/retify.py
@@ -5,33 +5,33 @@ import threading
 class StereoRectifier:
     def __init__(self):
         # Intrinsic parameters for both cameras
-        self.intrinsics_cam0 = np.array([3431.6333053510584, 3425.4233661235844, 1037.0631103881203, 814.2748160563247])
-        self.intrinsics_cam1 = np.array([3450.443379487295, 3442.6715206818894, 1000.8126349532237, 768.292008152334])
+        self.intrinsics_cam0 = np.array([3315.158744682164, 3317.4423656635427, 1044.9919174051424, 769.2791390484149])
+        self.intrinsics_cam1 = np.array([3303.1303751889086, 3304.4918413538717, 1021.6391642423999, 738.8164474717523])
 
         # Transformation matrix between cameras
         self.T_cn_cnm1 = np.array([
-            [0.9954706102064841, -0.002938725639798721, 0.09502435533453286, -0.1898177118318758],
-            [0.002702420629833145, 0.999992928337077, 0.0026153773296546125, -0.001517588623992596],
-            [-0.09503136923073889, -0.002346735488078846, 0.9954715122466737, 0.01757994327060201],
+            [0.9958785322848419, -0.001883418317043302, 0.09067745954553587, -0.19167885361868625],
+            [0.0021215517599516157, 0.9999945494395888, -0.0025298437192514665, -0.0007459795575273124],
+            [-0.09067220054856458, 0.0027117939739249486, 0.9958770999581846, 0.006433227010261678],
             [0.0, 0.0, 0.0, 1.0]
         ])
 
         # Distortion coefficients for both cameras
-        self.distortion_coeffs_cam0 = np.array([-0.29763392127051924, 0.8835065755916732, -0.0023637256530460084, -0.0007121489154046612])
-        self.distortion_coeffs_cam1 = np.array([-0.2760132481960459, 0.7026370095608109, -0.0015314823046884355, -0.001072650889485783])
+        self.distortion_coeffs_cam0 = np.array([-0.26727533865087455, 0.6914547210589541, -0.00022631332732631944, 0.0006445742114670336])
+        self.distortion_coeffs_cam1 = np.array([-0.25984247075537714, 0.609867968623631, 0.0008865986409872509, -0.000975406239237423])
 
         # Extract rotation and translation
         R = self.T_cn_cnm1[:3, :3]  # Rotation matrix (3x3)
         T = self.T_cn_cnm1[:3, 3]   # Translation vector (3x1)
 
         # Intrinsic camera matrices
-        K1 = np.array([
+        self.K1 = np.array([
             [self.intrinsics_cam0[0], 0, self.intrinsics_cam0[2]],
             [0, self.intrinsics_cam0[1], self.intrinsics_cam0[3]],
             [0, 0, 1]
         ])
 
-        K2 = np.array([
+        self.K2 = np.array([
             [self.intrinsics_cam1[0], 0, self.intrinsics_cam1[2]],
             [0, self.intrinsics_cam1[1], self.intrinsics_cam1[3]],
             [0, 0, 1]
@@ -45,27 +45,26 @@ class StereoRectifier:
 
         # Stereo rectification
         self.R1_rect, self.R2_rect, self.P1, self.P2, self.Q, roi1, roi2 = cv2.stereoRectify(
-            K1, dist_coeffs1, K2, dist_coeffs2, self.image_size, R, T
+            self.K1, dist_coeffs1, self.K2, dist_coeffs2, self.image_size, R, T, flags=cv2.CALIB_ZERO_DISPARITY
         )
+        
 
         # Rectification maps
-        self.map1x, self.map1y = cv2.initUndistortRectifyMap(K1, dist_coeffs1, self.R1_rect, self.P1, self.image_size, cv2.CV_32F)
-        self.map2x, self.map2y = cv2.initUndistortRectifyMap(K2, dist_coeffs2, self.R2_rect, self.P2, self.image_size, cv2.CV_32F)
+        self.map1x, self.map1y = cv2.initUndistortRectifyMap(self.K1, dist_coeffs1, self.R1_rect, self.P1, self.image_size, cv2.CV_32F)
+        self.map2x, self.map2y = cv2.initUndistortRectifyMap(self.K2, dist_coeffs2, self.R2_rect, self.P2, self.image_size, cv2.CV_32F)
         
-        print("self.P1")
-        print(self.P1)
-        print("self.P2")
-        print(self.P2)
 
     def rectify_images(self, image_path_cam0, image_path_cam1):
         # Load images
         image_cam0 = cv2.imread(image_path_cam0)
         image_cam1 = cv2.imread(image_path_cam1)
         
-        # bin the image to 2012, 1518 from the 4k one
+        # bin the image to 2012, 1518 as the image i took was not the right res
         bin_factor = 2
         height, width = image_cam0.shape[:2]
         height, width = image_cam1.shape[:2]
+        
+        print(height, width)
         
         image_cam0 = cv2.resize(image_cam0, (width // bin_factor, height // bin_factor), interpolation=cv2.INTER_AREA)
         image_cam1 = cv2.resize(image_cam1, (width // bin_factor, height // bin_factor), interpolation=cv2.INTER_AREA)
@@ -73,6 +72,9 @@ class StereoRectifier:
         if image_cam0 is None or image_cam1 is None:
             print("Error: Could not load one or both images.")
             return
+
+        # undistorted_cam0 = cv2.undistort(image_cam0, self.K1, self.distortion_coeffs_cam0)
+        # undistorted_cam1 = cv2.undistort(image_cam1, self.K2, self.distortion_coeffs_cam1)
 
         # Apply rectification
         rectified_cam0 = cv2.remap(image_cam0, self.map1x, self.map1y, interpolation=cv2.INTER_LINEAR)
@@ -85,12 +87,12 @@ class StereoRectifier:
         cv2.destroyAllWindows()
 
         # Save images
-        cv2.imwrite('rectified_left2.jpg', rectified_cam0)
-        cv2.imwrite('rectified_right2.jpg', rectified_cam1)
+        cv2.imwrite('rectifiedT5_right6.jpg', rectified_cam0)
+        cv2.imwrite('rectifiedT5_left6.jpg', rectified_cam1)
 
         print("Rectified images saved successfully.")
 
 # Example usage
 if __name__ == "__main__":
     rectifier = StereoRectifier()
-    rectifier.rectify_images('camera_DEV_1AB22C00E123_image_2.png', 'camera_DEV_1AB22C00E588_image_2.png')
+    rectifier.rectify_images('camera_DEV_1AB22C00E123_imageT5_6.png', 'camera_DEV_1AB22C00E588_imageT5_6.png')
